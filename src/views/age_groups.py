a7e33fd8a5b1ee20260534574ee79fb71e5923ba
David Jetelina <david.jetelina@firma.seznam.cz>
2017-09-16 00:10:37 +0200

    typovani, odstraneni spec

--- src/views/age_groups.py
+++ src/views/age_groups.py
@@ -3,26 +3,23 @@
 from views.decorators import speaks_json
 from flask import request, current_app
 from util.db import common_db as db
+from typing import Dict, Union, List
+import sqlite3
 
-spec = {
-    'title': str,
-    'data': [{
-        'label': str,
-        'value': int
-    }]
-}
+
+return_dict_type = Dict[str, Union[str, List[Dict[str, Union[str, int]]]]]
 
 
 class AgeGroup:
     def __init__(self, district, gender='a'):
         self.district = str(district)
-        self.gender = gender
-        self._db_data = None
-        self.return_data = {'title': 'Věk obyvatelstva v okrsku', 'data': []}
+        self.gender: str = gender
+        self._db_data: List[sqlite3.Row] = None
+        self.return_data: return_dict_type = {'title': 'Věk obyvatelstva v okrsku', 'data': []}
         self._get_db_data()
         self._format_data()
 
-    def _get_db_data(self):
+    def _get_db_data(self) -> None:
         if self._db_data:
             return
         with db(cursor=True) as cur:
@@ -31,7 +28,7 @@ class AgeGroup:
         self._db_data = sorted([row for row in self._db_data if row['gender'] == self.gender],
                                key=lambda x: (x['age_start'] is None, x['age_start']))
 
-    def _format_data(self):
+    def _format_data(self) -> None:
         for row in self._db_data:
             if row['age_start'] is None:
                 entry = {'label': 'Celkem', 'value': int(row['count'])}
@@ -43,7 +40,7 @@ class AgeGroup:
 
 
 @speaks_json
-def age_groups_all():
+def age_groups_all() -> return_dict_type:
     """Vekove skupiny"""
     wanted_district = request.args.get('district_code')
     current_app.logger.debug('wanted district: %s', wanted_district)
