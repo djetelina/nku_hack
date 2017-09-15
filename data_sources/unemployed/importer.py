7d57fe2ba707264399e4d1476e9ee33d08c59dea
Kalinovsky Konstantin <konstantin.kalinovsky@firma.seznam.cz>
2017-09-15 20:40:40 +0200

    #! wip

--- data_sources/unemployed/importer.py
+++ data_sources/unemployed/importer.py
@@ -1,7 +1,41 @@
-# -*- coding utf-8 -*_
+# -*- coding utf-8 -*-
 
-from src import config
+import os
+import csv
+from src.util import db
+
+DATA_DIR = os.path.dirname(os.path.realpath(__file__)) + "/data/"
 
 
 def run():
-    print("ANO!")
+    with db.ruian_db() as ruian:
+        ruian.cursor()
+        """
+        CREATE TABLE unemployed IF NOT EXISTS (
+            municipality_id INT,
+            year INT,
+            month INT,
+            type VARCHAR
+        )
+        """
+
+    for fname in os.listdir(DATA_DIR):
+        if fname not in (".", ".."):
+            print(fname)
+            parse(DATA_DIR + fname)
+
+
+def parse(path):
+    with db.ruian_db() as ruian:
+        cur = ruian.cursor()
+        query_prefix = """INSERT INTO unemployed VALUES """
+        values = []
+        with open(path, 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            next(reader)
+            for row in reader:
+                values.append((
+                    row[1]
+                ))
+
+
