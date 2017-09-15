09b8d46336eaf3ed77410d8d97240dfa9acf68c6
Tomas Pavlacky <tomas.pavlacky@firma.seznam.cz>
2017-09-16 00:34:26 +0200

    first graph with error

--- frontend/components/GraphUnemployed.js
+++ frontend/components/GraphUnemployed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import * as d3 from 'd3';
+import {BarChart} from 'react-easy-chart';
 
 
 import { Header } from 'semantic-ui-react';
@@ -10,7 +11,10 @@ class GraphUnemployed extends React.Component {
 
     constructor(props) {
         super(props);
-        debugger;
+    }
+
+    componentDidMount() {
+        //var test = d3.histogram(this.props);
     }
 
     renderGraph() {
@@ -18,8 +22,10 @@ class GraphUnemployed extends React.Component {
             return (
                 <div>
                     <Header as='h4'>Nezaměstnanost</Header>
-
-
+                    <div id="graph-unemployed" />
+                    <BarChart
+                        data={this.props.data}
+                    />
                 </div>
             );
         }
