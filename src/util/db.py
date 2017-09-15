fa3f3fadafadfbca1dfb016418ef337a317d8287
Viktor Lacina <viktor.lacina@firma.seznam.cz>
2017-09-15 20:39:52 +0200

    pokracovani

--- src/util/db.py
+++ src/util/db.py
@@ -4,30 +4,34 @@ import config
 
 class DB:
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, return_cursor=False):
         self.file_path = file_path
+        self.return_cursor = return_cursor
 
     def __enter__(self):
         self.connection = sqlite3.connect(self.file_path)
-        return self.connection
+        self.connection.row_factory = sqlite3.Row  # chceme vraceti dicty
+        return self.connection.cursor() if self.return_cursor else self.connection
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.connection:
             self.connection.close()
 
 
-def common_db():
+def common_db(cursor=False):
     """
     Vrati otevrenou spolecnou databazi.
+    :param cursor: zdalipak vraceti kurzor, nebo konexi
     :return: ctx manager
     """
-    return DB(config.DB_PATH)
+    return DB(config.DB_PATH, return_cursor=cursor)
 
 
-def ruian_db():
+def ruian_db(cursor=False):
     """
     Vrati otevrenou RUIAN db
+    :param cursor: zdalipak vraceti kurzor, nebo konexi
     :return: ctx manager
     """
-    return DB(config.RUIAN_PATH)
+    return DB(config.RUIAN_PATH, return_cursor=cursor)
 
--- src/views/suggest_locality.py
+++ src/views/suggest_locality.py
@@ -6,19 +6,85 @@ from util import db
 
 @speaks_json
 def suggest_locality():
+    # type: () -> Dict[str, Any]
     """
     Vyhleda lokalitu podle zadaneho retezce
     """
     query = request.args.get("query")
+    if len(query) < 3:
+        return dict(result=False, message=u"Příliš krátké zadání")
 
-    with db.ruian_db() as ruian:
-        c = ruian.cursor()
-        c.execute("""
-          SELECT u.nazev, u.kod as ulice_kod  
-          FROM ulice u 
-          WHERE u.nazev LIKE ?
-        """, (query, ))
-        all_ = c.fetchall()
-        print(all_)
+    with db.ruian_db(cursor=True) as ruian_cursor:
 
-    return dict(result=True, data={'query': query})
+        # Vezmeme si jich 11 a pouzijeme 10. Pak vime, jestli mame i vic :-)
+
+        # napred ulice
+        results = get_by_street(ruian_cursor, query)
+
+        if not results:
+            results = get_by_municipality_part(ruian_cursor, query)
+
+        data = {
+            'we_have_more': len(results) == 11,  # Mame jich vice
+            'results': results[:10]  # Prvnich 10
+        }
+
+    return dict(result=True, data=data)
+
+
+def get_by_street(c, query):
+    # type: (sqlite3.Cursor, str) -> List[Dict[str, Any]]
+    """
+    Najde lokality podle nazvu ulice.
+    :param c:
+    :param query:
+    :return: vyslledky :-)
+    """
+    c.execute("""
+          SELECT 
+            u.nazev as street_name, 
+            u.kod as street_id,
+            ob.nazev as mueunicipality_name,
+            ob.kod as municipality_code,
+            ok.nazev as district_name,
+            ok.kod as district_code,
+            k.nazev as region_name,
+            k.kod as region_id  
+          FROM ulice u
+          JOIN obce ob ON (lower(u.nazev) LIKE ? AND ob.kod = u.obec_kod)
+          JOIN okresy ok ON ok.kod = ob.okres_kod 
+          JOIN vusc k ON k.kod = ok.vusc_kod
+          ORDER BY k.nazev, ok.nazev, ob.nazev, u.nazev
+          LIMIT 11
+        """, ("{}%".format(query.lower()), ))
+
+    return [dict(x) for x in c.fetchall()]
+
+
+def get_by_municipality_part(c, query):
+    # type: (sqlite3.Cursor, str) -> List[Dict[str, Any]]
+    """
+    Najde lokality podle nazvu casti obce.
+    :param c:
+    :param query:
+    :return: vyslledky :-)
+    """
+    c.execute("""
+          SELECT 
+            co.nazev as municipality_part_name, 
+            co.kod as municipality_part_id,
+            ob.nazev as municipality_name,
+            ob.kod as municipality_code,
+            ok.nazev as district_name,
+            ok.kod as district_code,
+            k.nazev as region_name,
+            k.kod as region_id  
+          FROM casti_obci co
+          JOIN obce ob ON (lower(co.nazev) LIKE ? AND ob.kod = co.obec_kod)
+          JOIN okresy ok ON ok.kod = ob.okres_kod 
+          JOIN vusc k ON k.kod = ok.vusc_kod
+          ORDER BY k.nazev, ok.nazev, ob.nazev, co.nazev
+          LIMIT 11
+        """, ("{}%".format(query.lower()), ))
+
+    return [dict(x) for x in c.fetchall()]
