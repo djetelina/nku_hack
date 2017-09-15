4fdeece6130bfe5e947d0da25f42e6d4d20df44f
Dan Pressl <dan.pressl@firma.seznam.cz>
2017-09-15 23:34:20 +0200

    Pracovni commit 4

--- data_sources/csu_obyvatelstvo/script.py
+++ data_sources/csu_obyvatelstvo/script.py
@@ -2,37 +2,68 @@
 
 import csv
 import sqlite3
+from pprint import pprint
 
 YEAR = '2011'
+SOURCE_FILE = 'data_sources/csu_obyvatelstvo/SLDB_OBYVATELSTVO.CSV'
 
 
-def save_to_db(table_name, data, types, ruain_data):
+def save_to_db(table_name, data, metrics):
     """
     Ulozi data do mezidatabaze, ze ktere si pak vytvorim dump a ten ulozim do repozitare.
     """
+    print('Ukládám do DB.')
     connection = sqlite3.connect('{}.sqlite'.format(table_name))
 
     # Kraj
+    connection.execute('''DROP TABLE IF EXISTS {}_kraj'''.format(table_name))
     connection.execute('''
-      CREATE TABLE {}_kraj (ID region_id, ID metric_id, TEXT metric, INT year DEFAULT 2011, REAL value)
+      CREATE TABLE {}_kraj (region_id INT, metric_id INT, metric TEXT, year INT DEFAULT 2011, value REAL)
     '''.format(table_name))
 
+    query_values = []
+    for region_id, values in data['kraj'].items():
+        for index, item in enumerate(values['data']):
+            metric = metrics[index]
+            query_values.append((region_id, metric[0], metric[2], item))
+    connection.executemany(
+        '''INSERT INTO {}_kraj (region_id, metric_id, metric, value) VALUES (?, ?, ?, ?)'''.format(table_name),
+        query_values
+    )
+
     # Okres
+    connection.execute('''DROP TABLE IF EXISTS {}_okres'''.format(table_name))
     connection.execute('''
-          CREATE TABLE {}_okres (ID region_id, ID district_id, TEXT metric_id, TEXT metric, INT year DEFAULT 2011, REAL value)
+          CREATE TABLE {}_okres (region_id INT, district_id INT, metric_id TEXT, metric TEXT, year INT DEFAULT 2011, value REAL)
         '''.format(table_name))
 
-    # Kraj
+    query_values = []
+    for district_id, values in data['okres'].items():
+        for index, item in enumerate(values['data']):
+            metric = metrics[index]
+            query_values.append((values['kraj'], district_id, metric[0], metric[2], item))
+    connection.executemany(
+        '''INSERT INTO {}_okres (region_id, district_id, metric_id, metric, value) VALUES (?, ?, ?, ?, ?)'''.format(table_name),
+        query_values
+    )
+
+    # Obec
+    connection.execute('''DROP TABLE IF EXISTS {}_obec'''.format(table_name))
     connection.execute('''
-          CREATE TABLE {}_obec (ID region_id, ID district_id, ID city_id, TEXT metric_id, TEXT metric, INT year DEFAULT 2011, REAL value)
+          CREATE TABLE {}_obec (region_id INT, district_id INT, city_id INT, metric_id TEXT, metric TEXT, year INT DEFAULT 2011, value REAL)
         '''.format(table_name))
 
+    query_values = []
+    for city_id, values in data['obec'].items():
+        for index, item in enumerate(values['data']):
+            metric = metrics[index]
+            query_values.append((values['kraj'], values['okres'], city_id, metric[0], metric[2], item))
+    connection.executemany(
+        '''INSERT INTO {}_obec (region_id, district_id, city_id, metric_id, metric, value) VALUES (?, ?, ?, ?, ?, ?)'''.format(table_name),
+        query_values
+    )
 
-def load_ruian_data():
-    """
-    Nacte data z nasi lokalni RUIAN databaze aby jsme mohli nazvy prevest na idcka.
-    """
-    pass
+    connection.commit()
 
 
 def load_list_location():
@@ -46,13 +77,14 @@ def load_list_location():
             kod: text,
         },
         okres: {
-            kod: [<kraj_nazev>, text]
+            kod: [<kraj_kod>, text]
         },
         obec: {
-            kod: [<kraj_nazev>, <okres_nazev>, text]
+            kod: [<kraj_kod>, <okres_kod>, text]
         }
     }
     """
+    print('Načítám seznamy území.')
     with open('data_sources/csu_obyvatelstvo/seznam_uzemi.CSV', 'r', encoding='cp1250') as f:
         reader = csv.reader(f, delimiter=',', quotechar='\"')
 
@@ -89,6 +121,11 @@ def load_list_location():
                     'okres': item[7]
                 }
 
+    with open('data_sources/csu_obyvatelstvo/seznam_uzemi.CSV', 'r', encoding='cp1250') as f:
+        reader = csv.reader(f, delimiter=',', quotechar='\"')
+
+        header = next(reader)
+
         data = {
             'kraj': {},
             'okres': {},
@@ -98,24 +135,24 @@ def load_list_location():
         for item in reader:
             if item[1] == '100':
                 # kraje
-                region = data_connection['kraj'][item[2]]
+                region = data_connection['kraj'][item[0]]
                 data['kraj'][region['id']] = region['nazev']
             elif item[1] == '101':
                 # okresy
-                district = data_connection['okres'][item[2]]
+                district = data_connection['okres'][item[0]]
                 region = data_connection['kraj'][district['kraj']]
                 data['okres'][district['id']] = [region['id'], district['nazev']]
             elif item[1] == '43':
                 # obce
-                city = data_connection['obec'][item[2]]
+                city = data_connection['obec'][item[0]]
                 district = data_connection['okres'][city['okres']]
                 region = data_connection['kraj'][district['kraj']]
                 data['obec'][city['id']] = [region['id'], district['id'], city['nazev']]
-
         return data
 
 
-def create_data(reader, types, list_locations):
+def create_data(reader, metrics, list_locations):
+    print('vytvářím data pro uložení do DB.')
     data = {
         'kraj': {},
         'okres': {},
@@ -126,29 +163,25 @@ def create_data(reader, types, list_locations):
         # Chceme pouze obec, okres a kraj
         if line[2] in ('43', '100', '101'):
             # Ziskame si ze radku vsechna uzitecna data
-            line_data = [int(float(line[index])) for _, index in types]
+            line_data = [int(float(line[index])) for _ignore1, index, _ignore2 in metrics]
             if line[2] == '100':
                 # kraje
-                location = list_locations['kraj'][line[3]]
                 data['kraj'][line[3]] = {
-                    'nazev': location,
                     'data': line_data
                 }
-            elif line[1] == '101':
+            elif line[2] == '101':
                 # okresy
                 location = list_locations['okres'][line[3]]
                 data['okres'][line[3]] = {
                     'kraj': location[0],
-                    'nazev': location[1],
                     'data': line_data
                 }
-            elif line[1] == '43':
+            elif line[2] == '43':
                 # obce
                 location = list_locations['obec'][line[3]]
-                data['okres'][line[3]] = {
+                data['obec'][line[3]] = {
                     'kraj': location[0],
                     'okres': location[1],
-                    'nazev': location[2],
                     'data': line_data
                 }
 
@@ -156,12 +189,13 @@ def create_data(reader, types, list_locations):
 
 
 def nationality(list_locations):
-    with open('data_sources/csu_obyvatelstvo/SLDB_OBYVATELSTVO.CSV', 'r', encoding='cp1250') as f:
+    print('Zpracovávám národnost.')
+    with open(SOURCE_FILE, 'r', encoding='cp1250') as f:
         reader = csv.reader(f, delimiter=',', quotechar='\"')
 
         header = next(reader)
 
-        types = [
+        metrics = [
             ('czech', header.index('vse4121'), 'Česká'),
             ('moravia', header.index('vse4131'), 'Moravská'),
             ('slez', header.index('vse4141'), 'Slezská'),
@@ -174,35 +208,36 @@ def nationality(list_locations):
             ('unknown', header.index('vse41111'), 'Neuvedená'),
         ]
 
-        data = create_data(reader, types, list_locations)
-
-        return data
+        data = create_data(reader, metrics, list_locations)
+        save_to_db('nationality', data, metrics)
 
 
 def marital_status(list_locations):
-    with open('data_sources/csu_obyvatelstvo/SLDB_OBYVATELSTVO.CSV', 'r', encoding='cp1250') as f:
+    print('Zpracovávám rodinný stav')
+    with open(SOURCE_FILE, 'r', encoding='cp1250') as f:
         reader = csv.reader(f, delimiter=',', quotechar='\"')
 
         header = next(reader)
 
-        types = [
+        metrics = [
             ('single', header.index('vse1121'), 'Svobodní'),
             ('maried', header.index('vse1131'), 'Ženatí'),
             ('divorced', header.index('vse4121'), 'Rozvedení'),
             ('widowed', header.index('vse4121'), 'Ovdovělí'),
         ]
 
-        data = create_data(reader, types, list_locations)
-        return data
+        data = create_data(reader, metrics, list_locations)
+        save_to_db('marital_status', data, metrics)
 
 
 def education(list_locations):
-    with open('data_sources/csu_obyvatelstvo/SLDB_OBYVATELSTVO.CSV', 'r', encoding='cp1250') as f:
+    print('Zpracovávám vzdělání')
+    with open(SOURCE_FILE, 'r', encoding='cp1250') as f:
         reader = csv.reader(f, delimiter=',', quotechar='\"')
 
         header = next(reader)
 
-        types = [
+        metrics = [
             ('base', header.index('vse2131'), 'Základní'),
             ('apprenticeship', header.index('vse2141'), 'Vyučení'),
             ('high', header.index('vse2151'), 'Středoškolské'),
@@ -211,11 +246,12 @@ def education(list_locations):
             ('university', header.index('vse2181'), 'Vysokoškolské'),
         ]
 
-        data = create_data(reader, types, list_locations)
-
-        return data
+        data = create_data(reader, metrics, list_locations)
+        save_to_db('education', data, metrics)
 
 
 if __name__ == '__main__':
     locations_list = load_list_location()
-    nationality_data = nationality(locations_list)
+    nationality(locations_list)
+    marital_status(locations_list)
+    education(locations_list)
