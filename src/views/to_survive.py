a58e5d6ae62a79e9bcbf3a64eac0dee13408cf75
Viktor Lacina <viktor.lacina@firma.seznam.cz>
2017-09-16 14:16:59 +0200

    udrzba

--- src/views/to_survive.py
+++ src/views/to_survive.py
@@ -24,7 +24,7 @@ def to_survive():
     district_code = request_data.get("district_code")
 
     response = {
-        "result": False,
+        "result": True,
         "data": {
             "title": "Střední délka života",
             "data": None
@@ -33,9 +33,9 @@ def to_survive():
 
     # chybi nam id okresku
     if not region_id or not district_code:
-        response['error'] = "'region_id' or 'district_code' are missing"
+        response['message'] = "'region_id' or 'district_code' are missing"
+        response["result"] = False
         return response
-
 
     result = get_data_from_query(region_id, district_code)
     for row in result:
@@ -46,9 +46,7 @@ def to_survive():
 
     response['data']['data'] = result
 
-
     return response
-
 
 
 def get_data_from_query(region_id, district_code):
