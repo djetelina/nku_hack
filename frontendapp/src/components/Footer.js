7aea53dc02872339cfae6b6de9f91f44273f87b0
Viktor Lacina <viktor.lacina@firma.seznam.cz>
2017-09-16 17:41:52 +0200

    credits

--- frontendapp/src/components/Footer.js
+++ frontendapp/src/components/Footer.js
@@ -10,10 +10,45 @@ const containerStyle = {
     zIndex: 999,
 };
 
-const FooterComp = () => (
-    <div style={containerStyle}>
-        Vzniklo v rámci hacktathonu NKU.
-    </div>
-);
+class FooterComp extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showDetail: false,
+        };
+    }
+
+    toggleDetail() {
+        this.setState({
+           showDetail: !this.state.showDetail
+        });
+    }
+
+    renderDetail() {
+        if (!this.state.showDetail) return null;
+        return (
+            <div style={{border: 'solid 2px gray', borderRadius: 8, padding: 6, background: '#EEEEEE'}}>
+                Výslovně to zprasili tito:
+                <ul>
+                    <li>Daňhel Ňuff Prezzl</li>
+                    <li>Vitezslaw Čumáček Lacsina</li>
+                    <li>Týnek Kostík Kalinowski</li>
+                    <li>Tomislav Topa Pawlaczki</li>
+                    <li>Daffid Jetel Jethellina</li>
+                </ul>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div style={containerStyle}>
+                {this.renderDetail()}
+                <span onClick={this.toggleDetail.bind(this)} style={{cursor: 'pointer'}}>Vzniklo pod vlivem spánkového deficitu na hackathonu NKU.</span>
+            </div>
+        );
+    }
+}
 
 export default FooterComp;
