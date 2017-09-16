f89270b279b34258b07688ef574b5e3983b4914d
Kalinovsky Konstantin <konstantin.kalinovsky@firma.seznam.cz>
2017-09-16 03:59:21 +0200

    #! unemployed

--- src/views/unemployed.py
+++ src/views/unemployed.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python
 # coding=utf-8
+import json
 
-
-from views.decorators import speaks_json
 from flask import request
 from util import db
-
+from views.decorators import speaks_json, allowed_post_only
 
 __all__ = ('unemployed',)
 
 TYPE_ALL = "NEZ0004"
-TYPE_WOMEN = "NEZ0006"
 
-COLOR_WOMEN = "#F00"
-COLOR_MEN = "#F0F"
-COLOR_ALL = "#0FF"
+COLOR_ALL = "#0F0"
 
 LIMIT = 10
 
 
 @speaks_json
+@allowed_post_only
 def unemployed():
     """
     Vraci nezamestnanost pro obec
-
-    :return:
     """
-    municipality_code = request.args.get("municipality_code")
+    municipality_code = json.loads(request.data).get("municipality_code")
+
     response = {
         "result": False,
         "data": {
@@ -40,20 +36,10 @@ def unemployed():
         response['error'] = "'municipality_code' is missing"
         return response
 
-    all = get_q(municipality_code, TYPE_ALL, COLOR_ALL, "v")
-    women = get_q(municipality_code, TYPE_WOMEN, COLOR_WOMEN, "z")
-
-    # muzi jsou rozdil mezi vsichni a zeny
-    men = [{
-        "y": all[i].get("y") - women[i].get("y"),
-        "x": all[i].get("x")[:-1] + "m",
-        "color": COLOR_MEN
-    } for i in range(LIMIT)]
+    all = get_data_from_query(municipality_code, TYPE_ALL, COLOR_ALL)
 
     combined = []
-    for i in range(LIMIT):
-        combined.append(women[i])
-        combined.append(men[i])
+    for i in reversed(range(LIMIT)):
         combined.append(all[i])
 
     response['data']['data'] = combined
@@ -61,7 +47,7 @@ def unemployed():
     return response
 
 
-def get_q(municipality_code, type_, color, key):
+def get_data_from_query(municipality_code, type_, color):
     # type: (str, str) -> list(dict)
     """
     Vytazeni poslednich 10ti zaznamu z tabulky nezamestnanosti
@@ -74,14 +60,14 @@ def get_q(municipality_code, type_, color, key):
         query = """
             SELECT
                 value_ AS y,
-                year_ || "-" || month_ || ? AS x,
+                year_ || "-" || month_ AS x,
                 ? AS color
             FROM unemployed
             WHERE
                 municipality_id = ? AND
                 type_ = ?
-            ORDER BY x ASC
+            ORDER BY year_ DESC, month_ DESC
             LIMIT ?
         """
-        cur.execute(query, (key, color, municipality_code, type_, LIMIT))
+        cur.execute(query, (color, municipality_code, type_, LIMIT))
         return [dict(x) for x in cur.fetchall()]
